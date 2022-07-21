dfc6c110ce3f3720556d477214cbe3b1efa91543
Taaseen <taas2002@students.ubc.ca>
2022-07-20 20:58:08 -0700

    Changed initial starting time

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 function App() {
 	const [count, setCount] = useState(0);
   const [isActive, setIsActive] = useState(false);
-  const [seconds, setSeconds] = useState(10);
+  const [seconds, setSeconds] = useState(60);
 
   useEffect(() => {
     let timer = null;
